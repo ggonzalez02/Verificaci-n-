a5f1343438064e64f77c31bdd435a26937471983
Maricruz Campos <maricruzcv1803@gmail.com>
2025-03-20 12:16:29 -0600

    Update Banco_de_Registro.v

--- Proyecto1/Banco_de_Registro.v
+++ Proyecto1/Banco_de_Registro.v
@@ -9,8 +9,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module register_bank_8088(
-    input clk
-    input reset
+    input clk,
+    input reset,
     input wire en_write,           // 1: Habilita escritura, 0: Escritura inhabilitada
     input wire [2:0] reg_write,    // Registro a escribir
     input wire [15:0] write_data,  // Datos a escribir
@@ -18,8 +18,8 @@ module register_bank_8088(
     input wire [2:0] reg_read2,    // Segundo registro de lectura
     input wire size,               // Tamaño del registro (0: 8 bits, 1: 16 bits)
     input wire select_high_low,    // Selección de XH o XL (0: parte baja, 1: parte alta )
-    output wire [15:0] read_data1, // Datos leídos del primer registro
-    output wire [15:0] read_data2  // Datos leídos del segundo registro
+    output reg [15:0] read_data1, // Datos leídos del primer registro
+    output reg [15:0] read_data2  // Datos leídos del segundo registro
 
 );
     // Registros de Uso General
@@ -37,39 +37,39 @@ module register_bank_8088(
         else if (en_write) begin
             if (size == 1) // Escribir en los registros de 16 bits
                 case (reg_write)
-                    4'h0: AX <= write_data;
-                    4'h1: BX <= write_data;
-                    4'h2: CX <= write_data;
-                    4'h3: DX <= write_data;
-                    4'h4: SP <= write_data;
-                    4'h5: BP <= write_data;
-                    4'h6: SI <= write_data;
-                    4'h7: DI <= write_data;
+                    3'h0: AX <= write_data;
+                    3'h1: BX <= write_data;
+                    3'h2: CX <= write_data;
+                    3'h3: DX <= write_data;
+                    3'h4: SP <= write_data;
+                    3'h5: BP <= write_data;
+                    3'h6: SI <= write_data;
+                    3'h7: DI <= write_data;
                     default;
                 endcase
-        end
+                
             else begin
                 case (reg_write) // Si se escribe en un registro de 8 bits, seleccionar parte alta o baja de los registros de uso general
-                    4'h0: begin
+                    3'h0: begin
                         if (select_high_low == 0)
                             AX[7:0] <= write_data[7:0];
                         else
                             AX[15:8] <= write_data[7:0];
                     end
-                    4'h1: begin
+                    3'h1: begin
                         if (select_high_low == 0)
                             BX[7:0] <= write_data[7:0];
                         else
                             BX[15:8] <= write_data[7:0];
                     end
-                    4'h2: begin
+                    3'h2: begin
                         if (select_high_low == 0)
                             CX[7:0] <= write_data[7:0];
                         else
                             CX[15:8] <= write_data[7:0];
                     end
-                    4'h3: begin
-                        if (select_high_low == 0)
+                    3'h3: begin
+                       if (select_high_low == 0)
                             DX[7:0] <= write_data[7:0];
                         else
                             DX[15:8] <= write_data[7:0];
@@ -77,31 +77,32 @@ module register_bank_8088(
                 endcase
             end
     end
+    end
 
     always(*) begin 
         case(reg_read1) //Lectura de Registro 1
-            4'h0: AX = read_data1;
-            4'h1: BX = read_data1;
-            4'h2: CX = read_data1;
-            4'h3: DX = read_data1;
-            4'h4: SP = read_data1;
-            4'h5: BP = read_data1;
-            4'h6: SI = read_data1;
-            4'h7: DI = read_data1;
-            default read_data1 = 16'h0000;
+            3'h0: read_data1 = AX;
+            3'h1: read_data1 = BX;
+            3'h2: read_data1 = CX;
+            3'h3: read_data1 = DX;
+            3'h4: read_data1 = SP;
+            3'h5: read_data1 = BP;
+            3'h6: read_data1 = SI;
+            3'h7: read_data1 = DI;
+            default: read_data1 = 16'h0000;
 
         endcase
 
         case(reg_read2) //Lectura de Registro 2
-            4'h0: AX = read_data2;
-            4'h1: BX = read_data2;
-            4'h2: CX = read_data2;
-            4'h3: DX = read_data2;
-            4'h4: SP = read_data2;
-            4'h5: BP = read_data2;
-            4'h6: SI = read_data2;
-            4'h7: DI = read_data2;
-            default read_data2 = 16'h0000;
+            3'h0: read_data2 = AX;
+            3'h1: read_data2 = BX;
+            3'h2: read_data2 = CX;
+            3'h3: read_data2 = DX;
+            3'h4: read_data2 = SP;
+            3'h5: read_data2 = BP;
+            3'h6: read_data2 = SI;
+            3'h7: read_data2 = DI;
+            default: read_data2 = 16'h0000;
         endcase
     end
 
