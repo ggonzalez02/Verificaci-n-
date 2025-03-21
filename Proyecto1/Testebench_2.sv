62222f2706f8e2c43c694e99ddfaaed56cddb389
Maricruz Campos <maricruzcv1803@gmail.com>
2025-03-21 14:45:03 -0600

    Update Testebench_2.sv

--- Proyecto1/Testebench_2.sv
+++ Proyecto1/Testebench_2.sv
@@ -41,6 +41,7 @@ module register_bank_8088_tb_2;
     );
 
     typedef enum bit { read = 1'b0, write = 1'b1 } operacion;
+    bit FIN; // Señal para el scoreboard
 
 //TESTER
     // Generación del reloj
@@ -122,8 +123,59 @@ module register_bank_8088_tb_2;
                 else begin
                     reg_read1 = obtener_registro();
                     reg_read2 = obtener_registro();
+
+                    FIN = 1;
+                    #1;
+                    FIN = 0;
                 end
             end
     end: tester
+
+//SCOREBOARD
+    always@(posedge FIN) begin
+        bit [15:0] expected_data1;
+        bit [15:0] expected_data2;
+        expected_data1 = 16'h0000;
+        expected_data2 = 16'h0000;
+
+        // Calcular el valor esperado
+        if (en_write == 1) begin
+            if (size == 1) begin // Escritura de 16 bits
+                if (reg_write == reg_read1) // Se revisa en cual registro se escribió
+                    expected_data1 = write_data;
+                if (reg_write == reg_read2)
+                    expected_data2 = write_data;
+            end 
+            else begin // Escritura de 8 bits
+                if (reg_write == reg_read1) begin
+                    if (select_high_low == 0) begin
+                        expected_data1[7:0] = write_data[7:0];
+                    end
+                    else begin 
+                        expected_data1[15:8] = write_data[7:0];
+                    end
+                end
+                if (reg_write == reg_read2) begin
+                    if (select_high_low == 0) begin
+                        expected_data2[7:0] = write_data[7:0];
+                    end
+                    else begin
+                        expected_data2[15:8] = write_data[7:0];
+                    end
+                end
+            end
+        end
+    
+        // Verificar lectura del primer registro
+        if (expected_data1 != read_data1)
+            $error("FALLO: Reg1: %0h, Esperado: %0h, Obtenido: %0h", 
+                reg_read1, expected_data1, read_data1);
+               
+        // Verificar lectura del segundo registro
+        if (expected_data2 != read_data2)
+            $error("FALLO: Reg2: %0h, Esperado: %0h, Obtenido: %0h", 
+                reg_read2, expected_data2, read_data2);
+
+    end: scoreboard
 
 endmodule
