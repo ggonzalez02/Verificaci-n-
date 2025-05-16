3d3e381089d0b99567516fff81e2e38e5696fff2
Gabriel <gonzalezchavezgabriel79@gmail.com>
2025-05-16 10:07:02 -0600

    update

--- Proyecto 2/queue.v
+++ Proyecto 2/queue.v
@@ -7,11 +7,18 @@
 // Module Name: queue
 // Description: 
 //////////////////////////////////////////////////////////////////////////////////
-module queue (input EN, input CLK, input RST,);
-input  [7:0] DATA ;
-output [31:0] Data_Q;
-wire a, b , c ;
+module queue (
+    input EN;
+    input clk;
+    input rst;
+    input  [7:0] data ;
+    output [31:0] Data_Q;
+);
 
+// Data -> R1 -> R2 -> R3 -> R4
+wire [7:0] OUT1,OUT2,OUT3,OUT4 
+
+reg
 
 
 
--- Proyecto 2/segments.v
+++ Proyecto 2/segments.v
@@ -9,12 +9,42 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module segments (
+    //inputs
     input wire clk;
     input wire rst; 
-    output [15:0] segmento;
+    input write_en; 
+    input  [1:0] reg_select; // 00 CS, 01 ES, 10 DS, 11 SS
+    input  [15:0] data;
+
+    //outputs de segmentos 
+    output [15:0] cs_out;
+    output [15:0] ds_out;
+    output [15:0] ss_out;
+    output [15:0] es_out;
 );
 
+localparam CS = 2'b00;
+localparam DS = 2'b01;
+localparam SS = 2'b10;
+localparam ES = 2'b11;
 
-
+always @(posedge clk or negedge rst ) begin
+    if (!rst) begin
+        cs_out <= 16'h0000;
+        ds_out <= 16'h0000;
+        ss_out <= 16'h0000;
+        es_out <= 16'h0000;
+        
+    end 
+    `elsif (write_en) begin
+        case (reg_select)
+        CS: cs_out <= data;
+        DS: ds_out <= data;
+        SS: ss_out <= data;
+        ES: es_out <= data;
+        endcase 
+    end 
+    
+end
 
 endmodule
