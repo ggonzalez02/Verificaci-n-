7dc7abdb29948e3ace3fd9ab5d5a04588bbfeb04
Gabriel <gonzalezchavezgabriel79@gmail.com>
2025-05-23 10:40:38 -0600

    update

--- Proyecto 2/Testbench_2.sv
+++ Proyecto 2/Testbench_2.sv
@@ -14,18 +14,23 @@
 // Probar la ALU 
 // Escribir a IP 
 // Shift en el queue 
-
+    bit [15:0] banc_seg[8]; // 4 segmentos
+    bit [7:0]  reg_IP;      // Registro IP
+    bit [7:0]  D_queue[4]; // 4 registros del queue
 include "interface.sv"
 
 module 8088Interface_tb2;
     
     //Interfaz
     interface_8088 bfm();
+
+    initial begin
+        bfm.clk = 0;
+        forever #5 bfm.clk = ~bfm.clk; // 10ns
+    end 
     
     //Tester
-    funtion 
-
-    endfunction 
+   
     //Scoreboard
 
 
@@ -40,7 +45,7 @@ module 8088Interface_tb2;
         .Data_regs(bfm.Data_regs),
         .RD_WR_seg(bfm.RD_WR_seg),
         .Data_seg(bfm.Data_seg),
-        
+        .Reg_Write(bfm.Reg_Write),
 
 
 
@@ -50,11 +55,37 @@ module 8088Interface_tb2;
 
 endmodule
 
- class Scorebaord; 
+class Scoreboard; 
     bit [15:0] banc_reg[8]; // 8 registros
-    bit [15:0] banc_seg[8]; // 4 segmentos
-    bit [7:0]  reg_IP;      // Registro IP
-    bit [7:0]  D_queue[4]; // 4 registros del queue
+    
+    
+    // función escribe al registro 
+    function void WR_reg(int indice,bit [15:0] data);
+        banc_reg[indice] = data;
+    endfunction
+    // función lee el registro
+    function bit RD_reg(int indice,bit [15:0] data_actual;);
+        return (data_actual === banc_reg[indice]);
+    endfunction
+
+endclass
 
 
- endclass
+class Tester; 
+    virtual interface_8088 bfm;
+    Scoreboard scb;
+    
+    
+
+
+    task WR_reg(int indice, bit [15:0] data);
+        bfm.RD_WR_Regs = 1;
+        bfm.Reg_Write = indice;
+        bfm.Data_Regs = data;
+        @(posedge bfm.clk);
+        @(posedge bfm.clk);
+        bfm.RD_WR_Regs = 0;
+        scb.WR_reg(indice, data);
+    endtask
+
+endclass
