7c01c748230b7af29b555818122857378fd69944
Gabriel <gonzalezchavezgabriel79@gmail.com>
2025-05-23 16:41:22 -0600

    update

--- Proyecto 2/Testbench_2.sv
+++ Proyecto 2/Testbench_2.sv
@@ -20,14 +20,8 @@ module Interface_tb2;
     
     //Interfaz
     interface_8088 bfm();
-
-
-    
     //Tester
-   
     //Scoreboard
-
-
     //DUT
     top DUT ( 
         // Mismos que hay en el top.v
@@ -38,12 +32,41 @@ module Interface_tb2;
         .Reg2(bfm.Reg2),
         .RD_WR_Regs(bfm.RD_WR_Regs),
         .Data_Regs(bfm.Data_Regs),
-        .RD_WR_seg(bfm.RD_WR_seg),
-        .Data_seg(bfm.Data_seg),
+        .RD_WR_Segments(bfm.RD_WR_Segments),
+        .Data_Segments(bfm.Data_Segments),
         .Reg_Write(bfm.Reg_Write),
-        .Reg_Read(bfm.Reg_Read),
         .Direction(bfm.Direction)
     );
+
+        
+    // ADD HERE:
+    initial begin
+        Scoreboard scb;
+        Tester tester;
+        
+        // Create objects
+        scb = new();
+        tester = new(bfm);
+        tester.scb = scb;
+        
+        // Initialize signals
+        bfm.reset_interface();
+        
+        // Run tests
+        tester.WR_reg(0, 16'h1234);
+        tester.WR_reg(1, 16'h5678);
+        
+        // Verify
+        bit [15:0] data;
+        tester.RD_reg(0, data);
+        if(scb.RD_reg(0, data)) 
+            $display("PASS");
+        else 
+            $display("FAIL");
+        
+        #50 $finish;
+    end
+
 
 endmodule
 
@@ -66,6 +89,10 @@ endclass
 class Tester; 
     virtual interface_8088 bfm;
     Scoreboard scb;
+
+    function new(virtual interface_8088 p_bfm);
+        bfm = p_bfm;
+    endfunction
     
     task WR_reg(int indice, bit [15:0] data);
         bfm.RD_WR_Regs = 1;
