535f7a8550feb5f4e3329e281312e3b899a4a063
Gabriel <gonzalezchavezgabriel79@gmail.com>
2025-05-23 07:04:56 -0600

    update

--- Proyecto 2/Testbench_2.sv
+++ Proyecto 2/Testbench_2.sv
@@ -8,27 +8,53 @@
 // Description: Testbench para probar la interfaz del 8088
 //////////////////////////////////////////////////////////////////////////////////
 
-`include "interface.sv"
+
+// Que quier testear? 
+//Quiero probar bancos de registros escribir y leer a AX, BX , CX, al igual que los segmentos CS,SS etc 
+// Probar la ALU 
+// Escribir a IP 
+// Shift en el queue 
+
+include "interface.sv"
 
 module 8088Interface_tb2;
     
     //Interfaz
-    8088Interface bfm();
+    interface_8088 bfm();
     
     //Tester
+    funtion 
 
+    endfunction 
     //Scoreboard
+
 
     //DUT
     top DUT (
-    .clk(bfm.clk),
-    .reset(bfm.reset),
-    .OP(bfm.OP),
-    .Reg1(bfm.Reg1),
-    .Reg2(bfm.Reg2),
-    .RD_WR(bfm.RD_WR_pin),
-    .Data(bfm.Data_pin),
-    ,Direction(bfm.Direction)
+        .clk(bfm.clk),
+        .reset(bfm.reset),
+        .OP(bfm.OP),
+        .Reg1(bfm.Reg1),
+        .Reg2(bfm.Reg2),
+        .RD_WR_regs(bfm.RD_WR_regs),
+        .Data_regs(bfm.Data_regs),
+        .RD_WR_seg(bfm.RD_WR_seg),
+        .Data_seg(bfm.Data_seg),
+        
+
+
+
+
+        .Direction(bfm.Direction)
     );
 
 endmodule
+
+ class Scorebaord; 
+    bit [15:0] banc_reg[8]; // 8 registros
+    bit [15:0] banc_seg[8]; // 4 segmentos
+    bit [7:0]  reg_IP;      // Registro IP
+    bit [7:0]  D_queue[4]; // 4 registros del queue
+
+
+ endclass
