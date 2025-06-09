3c986190b1c711eb8602bd200ef0dd57d87b5367
Maricruz Campos <maricruzcv1803@gmail.com>
2025-06-09 10:58:38 -0600

    Create fp_deco_A.v

---
+++ Proyecto 3/fp_deco_A.v
@@ -0,0 +1,22 @@
+//////////////////////////////////////////////////////////////////////////////////
+// University:  Instituto Tecnológico de Costa Rica                             
+// Engineers:   Anthony Artavia                                                 
+//              Maricruz Campos                                                 
+//              Gabriel González                                                
+//  
+// Module Name: fp_decoA
+// Description: Este módulo se encarga de dividir un número de 32 bits de punto flotante en signo, exponente y mantissa.
+//////////////////////////////////////////////////////////////////////////////////
+
+module address (
+    input  [31:0] float_numA,
+    output signoA,
+    output [7:0] exponenteA,
+    output [23:0] mantissaA
+);
+
+assign signoA = float_numA[31];
+assign exponenteA = float_numA[30:23];
+assign mantissaA = { 1'b1, float_numA[22:0]};
+
+endmodule 
