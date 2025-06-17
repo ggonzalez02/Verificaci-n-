8f57378d181c6ea62606061e3f062eaceb708d9f
Maricruz Campos <maricruzcv1803@gmail.com>
2025-06-17 07:22:24 -0600

    Update Adder.v

--- Proyecto 3/Adder.v
+++ Proyecto 3/Adder.v
@@ -1,9 +1,31 @@
+//////////////////////////////////////////////////////////////////////////////////
+// University:  Instituto Tecnológico de Costa Rica                             
+// Engineers:   Anthony Artavia                                                 
+//              Maricruz Campos                                                 
+//              Gabriel González                                                
+//  
+// Module Name: Adder
+// Description: Módulo que maneja la suma y resta de las mantissas.
+//////////////////////////////////////////////////////////////////////////////////
+
 module Adder (
-    input Signo,
-    input [7:0] Exp_comun,
+    input SignoA,
+    input SignoB,
     input [25:0] Resul_Mantissa_A,
     input [25:0] Resul_Mantissa_B,
-    output[23:0] Suma_resul
+    output[26:0] Suma_resul,
+    output Signo_sum
 );
-    
+
+wire operacion = (SignoA == SignoB) ? 1 : 0; // 1: suma, 0: resta
+wire mantissa_mayor = (Resul_Mantissa_A >= Resul_Mantissa_B) ? 1 : 0; // 1: A > B, 0: A < B
+
+assign Suma_resul = (operacion) ? Resul_Mantissa_A + Resul_Mantissa_B :
+                    (mantissa_mayor) ? Resul_Mantissa_A - Resul_Mantissa_B :
+                    Resul_Mantissa_B - Resul_Mantissa_A;
+
+assign Signo_sum = (operacion) ? SignoA :
+                   (Resul_Mantissa_A >= Resul_Mantissa_B) ? SignoA : 
+                   SignoB;
+
 endmodule
