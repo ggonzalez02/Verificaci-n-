2d879aa3c845725393412204c8b0253b1b036d60
Maricruz Campos <maricruzcv1803@gmail.com>
2025-06-18 10:49:49 -0600

    Update Adder.v

--- Proyecto 3/Adder.v
+++ Proyecto 3/Adder.v
@@ -20,10 +20,13 @@ module Adder (
 wire operacion = (SignoA == SignoB) ? 1 : 0; // 1: suma, 0: resta
 wire mantissa_mayor = (Resul_Mantissa_A >= Resul_Mantissa_B) ? 1 : 0; // 1: A > B, 0: A < B
 
+// Según el tipo de operación se suma o se restan las mantissas
 assign Suma_resul = (operacion) ? Resul_Mantissa_A + Resul_Mantissa_B :
                     (mantissa_mayor) ? Resul_Mantissa_A - Resul_Mantissa_B :
                     Resul_Mantissa_B - Resul_Mantissa_A;
 
+// Si la operación es suma se asigna el signo A, ya que son iguales,
+// si es resta se verifica cuál mantissa es mayor para establcer el signo
 assign Signo_sum = (operacion) ? SignoA :
                    (Resul_Mantissa_A >= Resul_Mantissa_B) ? SignoA : 
                    SignoB;
