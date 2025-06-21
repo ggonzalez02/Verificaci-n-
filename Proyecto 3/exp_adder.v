8da54f83bc836a207524ec942e059a3017c7acf9
Maricruz Campos <maricruzcv1803@gmail.com>
2025-06-20 23:11:09 -0600

    Update exp_adder.v

--- Proyecto 3/exp_adder.v
+++ Proyecto 3/exp_adder.v
@@ -17,6 +17,6 @@ module exp_adder (
 wire signed [8:0] exp_real_A = $signed({1'b0, Exponente_A}) - 9'd127;
 wire signed [8:0] exp_real_B = $signed({1'b0, Exponente_B}) - 9'd127;
 
-assign Exp_resul = Exponente_A + Exponente_B;
+assign Exp_resul = exp_real_A + exp_real_B + 9'd127;
 
 endmodule 
